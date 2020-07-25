635990981c1b954ae1434a2c384b25c73437a5a3
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-25 23:49:47 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,9 +1,17 @@
 class Waiter
 
+  attr_accessor :name, :tenure
+  
+  @@all = []
+  
   def initialize(name, tenure)
     @name = name
     @tenure = tenure
+    @@all << self
   end
   
+  def self.all
+    @@all
+  end
   
 end
